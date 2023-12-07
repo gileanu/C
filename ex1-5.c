fb74a6dec82401d687673620fceac9e656980e14
Razvan Raileanu <gileanu@Gileanus-Mac-mini.local>
2023-12-07 22:07:26 +0200

    ex1-5

---
+++ ex1-5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // print the Fahrenheit - Celcius table using for loop
+    int fahr;
+    for (fahr = 300; fahr >= 0; fahr = fahr - 20)
+    {
+        printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr - 32));
+    }
+}
